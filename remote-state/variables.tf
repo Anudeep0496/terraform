6b571ac1f28189722e8dc2881e0dc8b9180d257f
Anudeep0496 <anudeep0496m@gmail.com>
2026-04-25 22:41:21 +0530

    Terraform Remote statefile

---
+++ remote-state/variables.tf
@@ -0,0 +1,17 @@
+variable "instance_type" {
+    default = "t3.micro"
+    type = string
+    description = "type of instance"
+}
+
+variable "project" {
+    default = "expense"
+}
+
+variable "component" {
+    default = "backend"
+}
+
+variable "environment" {
+    default = "dev"
+}
