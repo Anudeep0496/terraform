91067d9c594cb7abacac812faeeaa645f9deffd9
Anudeep0496 <anudeep0496m@gmail.com>
2026-04-25 21:28:42 +0530

    Terraform locals

---
+++ locals/ec2.tf
@@ -0,0 +1,32 @@
+resource "aws_instance" "expense" {
+    ami         = data.aws_ami.al2023.id
+    vpc_security_group_ids = [aws_security_group.allow_tls.id]
+    instance_type = "t3.micro"
+    key_name = "learningdevops"
+    tags = {
+        Name = "terraform-demo"
+    }
+}
+
+resource "aws_security_group" "allow_tls" {
+    name = "allow_tls"
+    description = "Creating a allow_tls sg"
+ 
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "allow_tls"
+    }
+}
