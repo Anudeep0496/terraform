2305cbe44b4da1f7917bede13cf8b3a99b2d51bb
Anudeep0496 <anudeep0496m@gmail.com>
2026-04-25 21:33:17 +0530

    Terraform locals

--- locals/ec2.tf
+++ locals/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "expense" {
     ami         = local.ami_id
     vpc_security_group_ids = [aws_security_group.allow_tls.id]
-    instance_type = "t3.micro"
+    instance_type = local.instance_type
     key_name = "learningdevops"
     tags = {
         Name = "terraform-demo"
--- locals/locals.tf
+++ locals/locals.tf
@@ -1,3 +1,4 @@
 locals {
   ami_id = data.aws_ami.al2023.id
+  instance_type = "t3.micro"
 }
