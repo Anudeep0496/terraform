b3c6286657d75b8135182cba050dacaacb5e6193
Anudeep0496 <anudeep0496m@gmail.com>
2026-04-25 22:36:49 +0530

    Terraform Remote statefile

--- remote-state/provider.tf
+++ remote-state/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 6.0"
     }
   }
+  backend "s3" {
+    bucket = "learning-devopsfrom-scratch"
+    key = "learning-terraform-devopstool"  # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
+    region = "us-east-1"
+    dynamodb_table = "terraform-table-devops-locking"
+    
+  }
 }
 
 # Configure the AWS Provider
